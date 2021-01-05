3183acef2c3b2903c0e9d90ee9b92ad8b833a989
Kristina Smotrova <smotrova-kristina@mail.ru>
2021-01-05 23:59:13 +0400

    11

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -1,23 +1,39 @@
 package ru.vsu.cs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String text = readSomething();
+    public static void main(String[] args) throws Exception {
+        int number = readSomething();
+
+        String text = readFile(number);
         StringBuilder resultText = getLengthWords(text);
-        printSomething(resultText);
+
+        writeFile(resultText, number);
+        printSomething(text, resultText);
     }
 
-    private static String readSomething() {
+    private static int readSomething() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter ur text: ");
-        return scanner.nextLine();
+        System.out.print("Enter the number of the file you want to open: ");
+        return scanner.nextInt();
     }
 
-    private static void printSomething(StringBuilder resultText) {
-        System.out.println("The final text: " + resultText);
+    private static String readFile(int number) {
+        String text = "";
+        File input = new File("src\\ru\\vsu\\cs\\file\\input" + number + ".txt");
+        try {
+            Scanner scanner = new Scanner(input);
+            text = scanner.nextLine();
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.err.println("File not found");
+            System.exit(1);
+        }
+        return text;
     }
 
     private static StringBuilder getLengthWords(String text) {
@@ -34,5 +50,15 @@ public class Main {
             }
         }
         return resultText;
+    }
+
+    private static void writeFile(StringBuilder resultText, int number) throws Exception {
+        FileWriter output = new FileWriter("src\\ru\\vsu\\cs\\file\\output" + number + ".txt");
+        output.write(String.valueOf(resultText));
+        output.close();
+    }
+
+    private static void printSomething(String text, StringBuilder resultText) {
+        System.out.println("Text from a text file: " + text + "\n" + "Text with the number of characters in the word: " + resultText);
     }
 }
